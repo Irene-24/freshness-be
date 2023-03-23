bc696fc2f4d26c7e47016c00a4b2bdbc7abc9b66
Irene-24 <okaforirenen@gmail.com>
2023-03-23 12:43:47 +0100

    more test setup

--- src/__tests__/routes/CustomerRoutes.spec.ts
+++ src/__tests__/routes/CustomerRoutes.spec.ts
@@ -3,14 +3,13 @@ import request from "supertest";
 import buildApp from "@/test-utils/test-app";
 import Context from "@/test-utils/context";
 import customerRoutes from "@/routes/customers.route";
-import { serializeError } from "serialize-error";
-
-// import UserRepo from "@/repo/User.repo";
 
 let context: Context;
+let app: request.SuperTest<request.Test>;
 
 beforeAll(async () => {
   context = await Context.build();
+  app = request(buildApp({ routers: customerRoutes }));
 });
 
 beforeEach(async () => {
@@ -23,15 +22,62 @@ afterAll(() => {
 
 describe("Customer routes", function () {
   it("registers a user with a unique email and password", (done) => {
-    request(buildApp({ routers: customerRoutes }))
+    app
       .post("/api/v1/customers")
       .send({ email: "test@email.com", password: "Test123$" })
       .set("Content-Type", "application/json")
       .set("Accept", "application/json")
       .expect((res) => {
         expect(res.body.message).toEqual("Created customer");
+        expect(res.body.data.id).toBeDefined();
         expect(res.statusCode).toEqual(201);
       })
       .end(done);
   });
+
+  it("fails to register a user with an existing email", (done) => {
+    app
+      .post("/api/v1/customers")
+      .send({ email: "test@email.com", password: "Test123$" })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .end(() => {
+        app
+          .post("/api/v1/customers")
+          .send({ email: "test@email.com", password: "Test123$" })
+          .set("Content-Type", "application/json")
+          .set("Accept", "application/json")
+          .expect((res) => {
+            expect(res.body.message).toContain(
+              "Customer exists with this email"
+            );
+            expect(res.statusCode).toEqual(400);
+          })
+          .end(done);
+      });
+  });
+
+  it("fails to register a user with an invalid password", (done) => {
+    app
+      .post("/api/v1/customers")
+      .send({ email: "test@email.com", password: "Test123$" })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .expect((res) => {
+        expect(res.body.message).toContain("Invalid request");
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.error).toBeDefined();
+      })
+      .end(done);
+  });
+
+  //update customer details
+
+  //change avatar url
+
+  //disable account
+
+  //reset password
+
+  //
 });
