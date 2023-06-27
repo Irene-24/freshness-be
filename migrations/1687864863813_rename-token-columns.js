2f1e57c22c7fc5a2aad6b069391b96c273335ea0
Irene-24 <okaforirenen@gmail.com>
2023-06-27 13:42:46 +0100

    fix issues for trigger and email token

--- migrations/1687864863813_rename-token-columns.js
+++ migrations/1687864863813_rename-token-columns.js
@@ -1,23 +1,59 @@
-/* eslint-disable camelcase */
+exports.up = async (pgm) => {
+  // Check if the column isVerified exists
+  const isVerifiedExists = await pgm.db.query(`
+    SELECT EXISTS (
+      SELECT 1
+      FROM information_schema.columns
+      WHERE table_name = 'users'
+      AND column_name = 'isVerified'
+    );
+  `);
 
-exports.shorthands = undefined;
+  if (isVerifiedExists.rows[0].exists) {
+    pgm.sql(`ALTER TABLE users RENAME COLUMN isVerified TO is_verified;`);
+  }
 
-exports.up = (pgm) => {
-  pgm.sql(`
-    ALTER TABLE user_tokens 
-        RENAME refreshToken TO refresh_token;
-        
-    ALTER TABLE user_tokens 
-        RENAME verifyToken TO verify_token;
-       
-        
-        `);
+  // Check if the column isEnabled exists
+  const isEnabledExists = await pgm.db.query(`
+    SELECT EXISTS (
+      SELECT 1
+      FROM information_schema.columns
+      WHERE table_name = 'users'
+      AND column_name = 'isEnabled'
+    );
+  `);
+
+  if (isEnabledExists.rows[0].exists) {
+    pgm.sql(`ALTER TABLE users RENAME COLUMN isEnabled TO is_enabled;`);
+  }
 };
 
-exports.down = (pgm) => {
-  pgm.sql(` ALTER TABLE user_tokens 
-        RENAME refresh_token TO refreshToken;
-        
-    ALTER TABLE user_tokens 
-        RENAME verify_token TO verifyToken;`);
+exports.down = async (pgm) => {
+  // Check if the column is_verified exists
+  const isVerifiedExists = await pgm.db.query(`
+    SELECT EXISTS (
+      SELECT 1
+      FROM information_schema.columns
+      WHERE table_name = 'users'
+      AND column_name = 'is_verified'
+    );
+  `);
+
+  if (isVerifiedExists.rows[0].exists) {
+    pgm.sql(`ALTER TABLE users RENAME COLUMN is_verified TO isVerified;`);
+  }
+
+  // Check if the column is_enabled exists
+  const isEnabledExists = await pgm.db.query(`
+    SELECT EXISTS (
+      SELECT 1
+      FROM information_schema.columns
+      WHERE table_name = 'users'
+      AND column_name = 'is_enabled'
+    );
+  `);
+
+  if (isEnabledExists.rows[0].exists) {
+    pgm.sql(`ALTER TABLE users RENAME COLUMN is_enabled TO isEnabled;`);
+  }
 };
